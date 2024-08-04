380b10953bda5b69db202d1c9dc3d26240b878d4
EylemAbz <140246327+EylemAbz@users.noreply.github.com>
2024-08-04 11:59:11 +0300

    OOP Master Challenge

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        Item coke = new Item("drınk","coke",1.50);
+//        coke.printItem();
+//        coke.setSize("LARGE");
+//        coke.printItem();
+//
+//        Item avocado = new Item("Topping","avocado",1.50);
+//        avocado.printItem();
+
+//        Burger burger = new Burger("regular",4.00);
+//        burger.addToppings("BACON","CHEESE","MAYO");
+//        burger.printItem();
+
+//          MealOrder regularMeal = new MealOrder();
+//          regularMeal.addBurgerTopping("BACON","CHEESE","MAYO");
+//          regularMeal.setDrinkSize("LARGE");
+//          regularMeal.printItemizedList();
+
+//        MealOrder secondMeal = new MealOrder("turkey", "7-up","chili");
+//        secondMeal.addBurgerTopping("LETTUCE","CHEESE","MAYO");
+//        secondMeal.setDrinkSize("SMALL");
+//        secondMeal.printItemizedList();
+
+        MealOrder deluxeMeal = new MealOrder("deluxe","7-up","chili");
+        deluxeMeal.addBurgerTopping("AVOCADO","BACON","LETTUCE","CHEESE","MAYO");
+        deluxeMeal.setDrinkSize("SMALL");
+        deluxeMeal.printItemizedList();
+    }
+}
